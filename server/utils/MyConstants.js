e56956ca7532e475e519d1c5117dbe817ead1b85
pt93tn <nicktaolaocuatui@gmail.com>
2024-07-16 12:16:53 +0700

    dss

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'phuthinhh.xehxtgj.mongodb.net',
+    DB_USER: 'shoppingonline',
+    DB_PASS: 'pt123456',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'nguyenphuthinh',
+    JWT_EXPIRES: '360000000', // in milliseconds
+    EMAIL_USER: 'nicktaolaocuatui@gmail.com', // gmail service
+    EMAIL_PASS: 'meis xpgq ksbq tehe'
+  };
+  module.exports = MyConstants;
